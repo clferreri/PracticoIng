0847a0ad129186511846b796b106213d1e51aa52
Cristian Ferreri <clferreri94@hotmail.com>
2019-09-19 18:12:30 -0300

    Se crea vista de productos y compras

--- PracticoIngenieria/Apps/GestionCompras/views.py
+++ PracticoIngenieria/Apps/GestionCompras/views.py
@@ -5,3 +5,11 @@ from PracticoIngenieria.Apps.GestionCompras.models import Persona, Producto, Com
 def listarPersonas(request):
     personas = Persona.objects.all()
     return render(request, 'personas.html', context={'personas': personas})
+
+def listarProductos(request):
+    productos = Producto.objects.all()
+    return render(request, 'productos.html', context={'productos': productos})
+
+def listarCompras(request):
+    compras = Compra.objects.all()
+    return render(request, 'compras.html', context={'compras': compras})
